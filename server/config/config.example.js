9116588e934bbc9f2f7f0a2002522aca58a78e9a
6XAM <rosenzweig.max@gmail.com>
2020-02-25 13:52:31 -0500

    Update config format

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,8 +1,10 @@
 //This file holds any configuration variables we may need
 //'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
 
-module.exports = {
+const config = {
     db: {
-        uri: '', //place the URI of your mongo database here.
+        uri: '', //uri here
     }
 };
+
+export default config;
