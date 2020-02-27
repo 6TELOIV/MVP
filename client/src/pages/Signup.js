1d50ef936dab3d3c3745fdc79d4580908ae2f93c
notoliver <47568829+notoliver@users.noreply.github.com>
2020-02-27 15:47:34 -0500

    main stuff

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,95 @@
 import React from 'react';
 import './Signup.css';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { KeyboardDatePicker } from "@material-ui/pickers";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: 'red',
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    ok:{
+        backgroundColor: 'white',
+        borderRadius: '5%'
+    }
+  }));
 
 function Signup(){
+    const classes = useStyles();
+
     return(
-        <div>
-            <h1>placeholder this is the signup page</h1>
+        <Container component="main" maxWidth="xs" className={classes.ok}>
+        <CssBaseline />
+        <div className={classes.paper}>
+            <Avatar className={classes.avatar}/>
+            <Typography component="h1" variant="h5">
+            We just need a few more things
+            </Typography>
+            <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                    autoComplete="fname"
+                    name="firstName"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                />
+                </Grid>
+                
+                <Grid item xs={12}>
+                <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                />
+                </Grid>
+                <Grid item xs={12}>
+                
+                </Grid>
+            </Grid>
+            
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+            >
+                Sign Up
+            </Button>
+            </form>
         </div>
+    </Container>
     );
 }
 
