220dbf27421722f4c0d6471da193f4dff5de57ec
Mark <mdrewry0@gmail.com>
2020-03-09 22:39:59 -0400

    implemented login using temp page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,6 +8,8 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
+
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -28,15 +30,23 @@ const useStyles = makeStyles(theme => ({
 function Login(props) {
   const classes = useStyles();
   const [email, changeEmail] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const [profileInfo, setProfileInfo] = useState();
   async function signInRequest(e) {
     e.preventDefault();
-    let profileInfo = {
+    let info = {
       email: email
     };
-    console.log(profileInfo);
-    await axios.post("/api/signin", profileInfo).then(response => {
-      if (response.status === 200) console.log(response.found);
-    });
+    let response = await axios.post("/api/signin", info);
+    if (response.status === 200) {
+      setProfileInfo(response.data);
+      setRedirect(true);
+    }
+  }
+  if (redirect) {
+    return (
+      <Redirect to={{ pathname: "/Temp", state: { data: profileInfo } }} />
+    );
   }
   return (
     <Container component="main" maxWidth="xs" className={classes.ok}>
@@ -44,7 +54,11 @@ function Login(props) {
       <div className={classes.paper}>
         <Typography variant="h3">Login</Typography>
 
-        <form className={classes.form} validate onSubmit={e => signInRequest(e)}>
+        <form
+          className={classes.form}
+          validate
+          onSubmit={e => signInRequest(e)}
+        >
           <Grid item xs={12}>
             <TextField
               fullWidth
