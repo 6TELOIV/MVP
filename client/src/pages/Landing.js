9afda3fa9f40f2de59d7e7812ccce70c147eac03
Mark <mdrewry0@gmail.com>
2020-03-09 23:12:03 -0400

    fixed previous deletion and implemented connection

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,107 +1,94 @@
-import React, {useState} from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import {TextField} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import "./Landing.css";
 
-
 const useStyles = makeStyles(theme => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '10%',
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "10%"
+  },
 
-    headline: {
-       textAlign: 'center',
-    },
+  headline: {
+    textAlign: "center"
+  },
 
-    avatar: {
-        margin: theme.spacing(3),
-        backgroundColor: '#D74C3D',
-    },
-    form: {
-        marginTop: theme.spacing(1),
-
-    },
-    button: {
-        margin: theme.spacing(3, 8, 4),
-        fontSize: 10,
-        textTransform: 'none',
-        fontFamily: ['Arial',
-                    'sans-serif',].join(','),
-
-    },
-    ok: {
-        backgroundColor: "white",
-        borderRadius: "5%"
-    },
-
+  avatar: {
+    margin: theme.spacing(3),
+    backgroundColor: "#D74C3D"
+  },
+  form: {
+    marginTop: theme.spacing(1)
+  },
+  button: {
+    margin: theme.spacing(3, 8, 4),
+    fontSize: 10,
+    textTransform: "none",
+    fontFamily: ["Arial", "sans-serif"].join(",")
+  },
+  ok: {
+    backgroundColor: "white",
+    borderRadius: "5%"
+  }
 }));
 
+export default function Landing() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
 
-export default function Landing(){
+  return (
+    <Container component="main" maxWidth="xs" className={classes.ok}>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar} />
 
-    const classes = useStyles();
-    const [email, setEmail] = useState('');
-
-    return(
-        <Container component="main" maxWidth="xs" className={classes.ok}>
-            <div  className={classes.paper}>
-
-                <Avatar className = {classes.avatar}/>
-
-                <Typography component = "h1"  variant="h5"  className={classes.headline} >
-                     Because you are the <br/>
-                    skies in ecstatic moon
-                 </Typography>
-                 <form className={classes.form} noValidate>
-                    <TextField
-                     label = "Email Address"
-                     margin = "normal"
-                     variant= "outlined"
-                     size = "small"
-                     value = {email}
-                     onChange = {e => setEmail(e.target.value)}
-                    />
-                     <br>
-                     </br>
-                    <Link to={
-                        {
-                            pathname: "Signup",
-                            email: email
-                        }
-                        }
-                          style={{ textDecoration: 'none' }}
-                            >
-                        <Button className={classes.button} variant = "outlined" size ="small">
-                        Sign up
-                        </Button>
-                    </Link>
-                    <Typography component="h3" className={classes.headline}>
-                    Already have an account?
-                    </Typography>
-                     <Link to={
-                         {
-                             pathname: "Login",
-                         }
-                     }
-                           style={{ textDecoration: 'none' }}
-                      >
-                    <Button className ={classes.button} variant = "outlined"  size ="small">
-                    Login
-                    </Button>
-                     </Link>
-                </form>
-
-            </div>
-        </Container>
-    );
+        <Typography component="h1" variant="h5" className={classes.headline}>
+          Because you are the <br />
+          skies in ecstatic moon
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            label="Email Address"
+            margin="normal"
+            variant="outlined"
+            size="small"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <br></br>
+          <Link
+            to={{
+              pathname: "Signup",
+              email: email
+            }}
+            style={{ textDecoration: "none" }}
+          >
+            <Button className={classes.button} variant="outlined" size="small">
+              Sign up
+            </Button>
+          </Link>
+          <Typography component="h3" className={classes.headline}>
+            Already have an account?
+          </Typography>
+          <Link
+            to={{
+              pathname: "Login"
+            }}
+            style={{ textDecoration: "none" }}
+          >
+            <Button className={classes.button} variant="outlined" size="small">
+              Login
+            </Button>
+          </Link>
+        </form>
+      </div>
+    </Container>
+  );
 }
-
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,133 +1,176 @@
-import React, {useState} from "react";
-import './Signup.css';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import { DatePicker, MuiPickersUtilsProvider, TimePicker } from "@material-ui/pickers";
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import DateFnsUtils from '@date-io/date-fns';
+import React, { useState } from "react";
+import "./Signup.css";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import {
+  DatePicker,
+  MuiPickersUtilsProvider,
+  TimePicker
+} from "@material-ui/pickers";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import DateFnsUtils from "@date-io/date-fns";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
 const useStyles = makeStyles(theme => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(3),
-      backgroundColor: '#D74C3D',
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(5, 0, 3),
-      backgroundColor: '#396384'
-    },
-    ok:{
-        backgroundColor: 'white',
-        borderRadius: '5%'
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(3),
+    backgroundColor: "#D74C3D"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(5, 0, 3),
+    backgroundColor: "#396384"
+  },
+  ok: {
+    backgroundColor: "white",
+    borderRadius: "5%"
+  }
+}));
+
+function Signup(props) {
+  const classes = useStyles();
+  const [selectedDate, handleDateChange] = useState(new Date());
+  const [name, changeName] = useState("");
+  const [email, changeEmail] = useState(props.location.email);
+  const [birthplace, changeBirthplace] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const [profileInfo, setProfileInfo] = useState();
+  async function signInRequest() {
+    let info = {
+      email: email
+    };
+    let response = await axios.post("/api/signin", info);
+    console.log(response.status);
+    if (response.status === 200) {
+      setProfileInfo(response.data);
+      setRedirect(true);
     }
-  }));
-
-function Signup(props){
-    const classes = useStyles();
-    const [selectedDate, handleDateChange] = useState(new Date());    
-    const [name, changeName] = useState(''); 
-    const [email, changeEmail] = useState(''); 
-    const [birthplace, changeBirthplace] = useState('');
-
-    return(
-        <Container component="main" maxWidth="xs" className={classes.ok}>
-        <CssBaseline />
-        <div className={classes.paper}>
-            <Avatar className={classes.avatar}/>
-            <Typography component="h1" variant="h5">
-              We just need a few more things
-            </Typography>
-            <form className={classes.form} validate onSubmit={() => console.log(selectedDate)}>
-            <Grid container spacing={2}>
-
-                <Grid item xs={12}>
-                <TextField
-                    autoComplete="fname"
-                    variant="outlined"                  
-                    fullWidth                    
-                    label="First Name"
-                    autoFocus
-                    value={name}
-                    onChange={(e)=>{changeName(e.target.value)}}
-                />
-                </Grid>
-                
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    label="Email Address"
-                    autoComplete="email"
-                    value={email}
-                    onChange={(e)=>{changeEmail(e.target.value)}}
-                />
-                </Grid>
-
-                <Grid item xs={12}>
-                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <DatePicker
-                      disableFuture
-                      openTo="year"
-                      format="MM/dd/yyyy"
-                      label="Date of Birth"
-                      views={["year", "month", "date"]}                      
-                      value={selectedDate}
-                      onChange={handleDateChange}
-                    />
-                  </MuiPickersUtilsProvider>
-                 
-                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <TimePicker 
-                      autoOk
-                      label="Time of Birth"
-                      value={selectedDate} 
-                      onChange={handleDateChange}
-                    />
-                  </MuiPickersUtilsProvider>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                      variant="outlined"
-                      required
-                      fullWidth
-                      label="Birthplace"
-                      value={birthplace} 
-                      onChange={(e)=>{changeBirthplace(e.target.value)}}
-                  />
-                </Grid>
-
-            </Grid>
-            
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-            >
-                Sign Up
-            </Button>
-
-            </form>
-        </div>
-    </Container>
+  }
+  async function signupRequest(e) {
+    e.preventDefault();
+    let info = {
+      email: email,
+      name: name,
+      address: birthplace,
+      birthday: Math.floor(selectedDate.getTime() / 1000)
+    };
+    let response = await axios.post("/api/signup", info);
+    if (response.status === 200) {
+      signInRequest();
+    }
+  }
+  if (redirect) {
+    return (
+      <Redirect to={{ pathname: "/Temp", state: { data: profileInfo } }} />
     );
+  }
+  return (
+    <Container component="main" maxWidth="xs" className={classes.ok}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar} />
+        <Typography component="h1" variant="h5">
+          We just need a few more things
+        </Typography>
+        <form
+          className={classes.form}
+          validate
+          onSubmit={e => signupRequest(e)}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="fname"
+                variant="outlined"
+                fullWidth
+                label="First Name"
+                autoFocus
+                value={name}
+                onChange={e => {
+                  changeName(e.target.value);
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Email Address"
+                autoComplete="email"
+                value={email}
+                onChange={e => {
+                  changeEmail(e.target.value);
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <DatePicker
+                  disableFuture
+                  openTo="year"
+                  format="MM/dd/yyyy"
+                  label="Date of Birth"
+                  views={["year", "month", "date"]}
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                />
+              </MuiPickersUtilsProvider>
+
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <TimePicker
+                  autoOk
+                  label="Time of Birth"
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Birthplace"
+                value={birthplace}
+                onChange={e => {
+                  changeBirthplace(e.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 }
 
 export default Signup;
