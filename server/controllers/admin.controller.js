fd128ab06f5697e2200ecb2ba84e9e0651607923
Shaun Fidler <shaunfidler@ufl.edu>
2020-03-12 19:25:59 -0400

    Setup base routing for admin page

---
+++ server/controllers/admin.controller.js
@@ -0,0 +1,12 @@
+import userModel from '../models/userModel.js';
+
+	export const view = async (req, res) => {
+		res.send('View').status(200);
+	}
+
+	export const edit = async(req, res) => {	
+		
+		
+		res.send('Edit').status(200);
+	}
+	
--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import * as accountController from '../controllers/account.controller.js';
 import coordinatesController from '../controllers/coordinates.controller.js'
+import * as adminController from '../controllers/admin.controller.js'
 const apiRouter = express.Router();
 
 apiRouter.post('/signup', coordinatesController, accountController.signUp);
 apiRouter.post('/signin', accountController.signIn)
+apiRouter.get('/admin', adminController.view)
+apiRouter.put('/admin', adminController.edit)
 
 export default apiRouter;
