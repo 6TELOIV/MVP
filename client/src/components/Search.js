c8e085c9c3ff410b623a17092652cb3b20cc9b69
Rovaldy <rapplyrs23@gmail.com>
2020-03-18 15:36:57 -0400

    Added search.js for filter

---
+++ client/src/components/Search.js
@@ -0,0 +1,26 @@
+
+/*import React, {useState} from 'react';
+
+
+
+export default function Search() {
+
+    const [filterText, setFilterText] = useState('');
+
+    const filterUpdate = (value) => {
+        
+        setFilterText(value);
+    }
+
+
+
+
+    return (
+        <form>
+            <input onChange={event => filterUpdate(event)} type="text" placeholder="Type to Filter"/>
+        </form>
+    );
+
+};
+
+*/
