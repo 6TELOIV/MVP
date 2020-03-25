298c4e5d56add377ef17a8cb5395894566adcad7
Rovaldy <rapplyrs23@gmail.com>
2020-03-25 19:12:05 -0400

    Updated Search.js

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,40 +3,97 @@ import React, {useState} from 'react';
 
 const Search = (props) => {
 
+//State Variables for different fields
     const [house, setHouseName] = useState('');
     const [sign, setSign] = useState('');
     const [text, setText] = useState('');
+    const [moon, setMoon] = useState(''); 
     
-    
+//Submit function that allows you to pass filtered fields from this component into filterUpdate function    
     const handleSubmit = () => {
 
         props.filterUpdate({
             house: house, 
             sign: sign, 
+            moon: moon, 
             text: text, 
         });
     }
-    
+//Filter function that filters the different fields and then sets the state variables     
     const filter = (event) => {
+
+    //House field
+        var val = event.target.value;
         
-        
-        var house_1 = event.substring(
-            event.lastIndexOf("h=") + 1, 
-            event.lastIndexOf(" ")
+        if (val.toLowerCase().indexOf("h=") === -1){
+
+           var house_1 = null; 
+        }
+        else {
+
+          house_1 = val.substring(
+            val.lastIndexOf("h=") + 1, 
+            val.lastIndexOf(" ")
+            
+           );
+           val.replace(house_1, ""); 
+           parseInt(house_1)
+        }
+
+       setHouseName(house_1)
+
+    //Sign field
+        if (val.toLowerCase().indexOf("s=") === -1){
+
+        var sign_1 = null; 
+        }
+
+        else {
+
+         sign_1 = val.substring(
+            val.lastIndexOf("s=") + 1, 
+            val.lastIndexOf(" ")
         );
 
-        setHouseName(house_1)
+        val.replace(sign_1, ""); 
+        parseInt(sign_1) 
 
-       var sign_1 = event.substring(
-            event.lastIndexOf("s=") + 1, 
-            event.lastIndexOf(" ")
-        );
+       } 
+
         setSign(sign_1)
 
-        var text_1 = event.substring(
-            event.lastIndexOf("t=") + 1, 
-            event.lastIndexOf(":")
-        );
+    //Moon field
+
+        if (val.toLowerCase().indexOf("m=") === -1){
+
+            var moon_1 = null; 
+            }
+        
+        else {
+            moon_1 = val.substring(
+                val.lastIndexOf("m=") + 1, 
+                val.lastIndexOf(" ")
+            );
+            val.replace(moon_1, ""); 
+            parseInt(moon_1) 
+
+            if (moon_1 > 8 || moon_1 < 1){
+                moon_1 = null; 
+            }
+        }
+
+        setMoon(moon_1); 
+    //Text field
+
+        if (val === -1){
+
+            var text_1 = null; 
+            }
+
+         else{
+            text_1 = val; 
+            }
+
         setText(text_1)
 
        
@@ -53,3 +110,4 @@ const Search = (props) => {
 };
 
 export default Search;
+ 
