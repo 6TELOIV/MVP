d7919d40d414915afc6c10e9ed618bdf154274f6
notoliver <47568829+notoliver@users.noreply.github.com>
2020-03-30 01:32:24 -0400

    consistent styling and master style

---
+++ client/src/assets/Style.js
@@ -0,0 +1,24 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "15px"
+    },
+    avatar: {
+      margin: theme.spacing(3),
+      backgroundColor: "#D74C3D"
+    },
+    form: {
+      marginTop: theme.spacing(3)
+    },
+    button: {
+      margin: theme.spacing(3, 0, 3),
+      backgroundColor: "#396384"
+    }
+  }));
+
+export default useStyles
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,41 +1,28 @@
 import React, { useState } from "react";
-import "./Login.css";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  submit: {
-    margin: theme.spacing(5, 10, 3),
-    backgroundColor: "#396384"
-  },
-  ok: {
-    backgroundColor: "white",
-    borderRadius: "5%"
-  }
-}));
+import "./Site.css";
+import { Card } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import useStyles from "../assets/Style.js"
 
 function Login(props) {
   const classes = useStyles();
   const [email, changeEmail] = useState("");
+  const [password, changePassword] = useState("");
   const [redirect, setRedirect] = useState(false);
   const [profileInfo, setProfileInfo] = useState();
   async function signInRequest(e) {
     e.preventDefault();
     let info = {
-      email: email
+      email: email,
+      password: password
     };
     let response = await axios.post("/api/signin", info);
     if (response.status === 200) {
@@ -49,16 +36,19 @@ function Login(props) {
     );
   }
   return (
-    <Container component="main" maxWidth="xs" className={classes.ok}>
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <div className={classes.paper}>
-        <Typography variant="h3">Login</Typography>
-
+      <Card className={classes.paper}>
+        <Avatar className={classes.avatar} />
+        <Typography component="h1" variant="h5" align="center">
+          Welcome Back
+        </Typography>
         <form
           className={classes.form}
           validate
           onSubmit={e => signInRequest(e)}
         >
+          <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
               fullWidth
@@ -70,17 +60,33 @@ function Login(props) {
               }}
             />
           </Grid>
+          <br></br>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Password"
+              autoComplete="password"
+              onChange={e => {
+                changePassword(e.target.value);
+              }}
+            />
+          </Grid>
 
-          <Button
-            type="submit"
-            className={classes.submit}
-            variant="contained"
-            color="primary"
-          >
-            Login
-          </Button>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Login
+            </Button>
+          </Grid>
+          </Grid>
         </form>
-      </div>
+      </Card>
     </Container>
   );
 }
