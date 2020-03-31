0816e54d4c0b30ea7fb97e6c5e4ea9a4acd80b26
Shaun Fidler <shaunfidler@ufl.edu>
2020-03-30 21:31:23 -0400

    Fixed adminEdit styling

--- client/src/pages/AdminEdit.js
+++ client/src/pages/AdminEdit.js
@@ -4,8 +4,7 @@ import { AppBar, Typography, Toolbar } from '@material-ui/core'
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import { Redirect } from "react-router-dom";
-import "./AdminPage.css"; //Possibly remove
-import "./Site.css";
+import "./AdminPage.css"; 
 import axios from 'axios';
 
 function AdminEdit(props) {
