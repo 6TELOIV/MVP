99363b866967c83cdd17d858df19ed8b1a8e5e01
Max Rosenzweig <rosenzweig.max@gmail.com>
2020-04-16 23:00:15 -0400

    init commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import * as express from './config/express.js';
-
+import cron from 'cron';
+import lune from 'lune';
 
 global.config = {
   db: {
@@ -25,6 +26,22 @@ async function start() {
   
   const app = express.init();
   app.listen(port, () => console.log(`Server now running on port ${port}!`));
+  
+  const weeklyHoroscopeJob = new cron.CronJob("*/10 * * * * *", () => {
+    let now = new Date();
+    let nextWeek = new Date(now.getTime());
+    nextWeek.setDate(now.getDate() + 7);
+    let upcomingPhases = lune.phase_range(now, nextWeek);
+    console.log(now, nextWeek, upcomingPhases);
+
+    now.setSeconds(now.getSeconds() + 5);
+    let newJob = new cron.CronJob(now, () => {
+      let now = new Date();
+      console.log(now.toLocaleTimeString());
+    });
+    newJob.start();
+  });
+  weeklyHoroscopeJob.start();
 }
 
 start();
