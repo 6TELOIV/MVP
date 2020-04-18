886ab022fc043d1cc017e421cd3d99e364260fe0
Max Rosenzweig <rosenzweig.max@gmail.com>
2020-04-17 20:37:33 -0400

    Finished Cronjob outline

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import * as express from './config/express.js';
-import cron from 'cron';
-import lune from 'lune';
+import startJobs from './helpers/cronjobs.js';
 
 global.config = {
   db: {
@@ -28,22 +27,8 @@ async function start() {
   const port = process.env.PORT || 5000;
   const app = express.init();
   app.listen(port, () => console.log(`Server now running on port ${port}!`));
-  
-  const weeklyHoroscopeJob = new cron.CronJob("*/10 * * * * *", () => {
-    let now = new Date();
-    let nextWeek = new Date(now.getTime());
-    nextWeek.setDate(now.getDate() + 7);
-    let upcomingPhases = lune.phase_range(now, nextWeek);
-    console.log(now, nextWeek, upcomingPhases);
 
-    now.setSeconds(now.getSeconds() + 5);
-    let newJob = new cron.CronJob(now, () => {
-      let now = new Date();
-      console.log(now.toLocaleTimeString());
-    });
-    newJob.start();
-  });
-  weeklyHoroscopeJob.start();
+  startJobs();
 }
 
 start();
